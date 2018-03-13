facd80a8bfe6c05f69bfd6f099f3388497fc1b94
Les Smith <lestsmith@googlemail.com>
2018-03-13 20:54:05 +0100

    Layout changes

--- ifelse.php
+++ ifelse.php
@@ -7,8 +7,9 @@
 <?php
 if ( 4 > 2 ) {echo '<p>Yes, 4 is greater then 2<br>' ; }
 
-if ( ( 4 > 2 ) && (8 > 4 ) )
-	{echo '4 is grater than 2 AND 8 is greater then 4<br>' ; }
+if ( ( 4 > 2 ) && (8 > 4 ) ) {
+	echo '4 is grater than 2 AND 8 is greater then 4<br>' ; 
+}
 
 if (4 > 8) {
 	echo '4 is greater than 8<br>' ;
