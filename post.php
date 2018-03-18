17cb9aeef72131049044cdd01ef5571bed4199f2
Les Smith <lestsmith@googlemail.com>
2018-03-18 22:00:19 +0100

    Forum files

---
+++ post.php
@@ -0,0 +1,29 @@
+<?PHP
+
+	session_start();
+
+	if (!isset($_SESSION['user_id']))
+	{
+		require('login_tools.php');
+		load();
+	}
+
+	$page_title = 'Post Message';
+	include("includes/header.html");
+
+	echo '<form action="post_action.php" method="POST" accept-charset="utf-8">
+		<p>Subject:<br>
+		<input type="text" name="subject" size="64"></p>
+		<p>Message:<br>
+		<textarea name="message" rows="5" columns="50">
+		</textarea></p>
+		<p><input type="submit" value="Submit"></p>
+		</form>';
+
+	echo '<p>
+		<a href="forum.php">Forum</a> |
+		<a href="shop.php">Shop</a> |
+		<a href="home.php">Home</a> |
+		<a href="goodbye.php">Logout</a></p>';
+
+?>
---
+++ post_action.php
@@ -0,0 +1,46 @@
+<?PHP
+	session_start();
+
+	require('login_tools.php');
+
+	if (!isset($_SESSION['user_id'])) {load();}
+
+	$page_title = "Post Error";
+	include('includes/header.html');
+
+	if($_SERVER['REQUEST_METHOD'] == 'POST')
+	{
+		if (empty($_POST['subject']))
+			{ echo '<p>Please enter a subject for this post.</p>';}
+
+		if (empty($_POST['message']))
+			{ echo '<p>Please enter a message for this post.</p>';}
+
+		if (!empty($_POST['subject']) && !empty($_POST['message']))
+		{
+			require('../connect_db.php');
+			$q = "insert into forum
+				(first_name, last_name, subject, message, post_date)
+				values(
+					'{$_SESSION[first_name]}',
+					'{$_SESSION[last_name]}',
+					'{$_POST[subject]}',
+					'{$_POST[message]}',
+					now())";
+			$r = mysqli_query($dbc, $q);
+
+			if (mysqli_affected_rows($dbc) != 1)
+			{
+				echo '<p>Error</p>' . mysqli_error($dbc);
+			}
+			else
+			{
+				load('forum.php');
+			}
+
+			mysqli_close($dbc);
+		}
+	}
+	echo '<p><a href="forum.php">Forum</a></p>';
+	include('includes/footer.html');
+?>
