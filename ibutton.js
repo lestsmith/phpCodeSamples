735239ea6580962ef87b9283a3c9986f0d98f678
Les Smith <lestsmith@googlemail.com>
2018-04-15 11:37:27 +0200

    More HTML files

---
+++ ibutton.js
@@ -0,0 +1,12 @@
+function send() {
+	var address = document.getElementById("adr").value;
+	var pattern = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+/;
+	if (!pattern.test(address)) alert("Invalid Email Addess");
+	else document.getElementById("form-1").submit();
+}
+
+function init() {
+	document.getElementById("btn").onclick=send;
+}
+
+onload=init;
